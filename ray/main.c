c9bf3baefe3e5f4b8f2fa3f49d558bd819978eea
Leo Garcia <leo.ward.garcia@gmail.com>
2024-11-30 15:06:15 -0800

    reloading on compile

--- ray/main.c
+++ ray/main.c
@@ -1,6 +1,10 @@
+#include <errno.h>
 #include <dlfcn.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include "raylib.h"
 
 #include "plug.h"
@@ -19,7 +23,14 @@ void libplug_reload(void)
 {
 	if (libplug)
 		dlclose(libplug);
-	libplug =  dlopen(lib_plug_file_name, RTLD_NOW);
+
+	for (size_t attempt = 0; attempt < 5; attempt++) {
+		libplug =  dlopen(lib_plug_file_name, RTLD_NOW);
+		if (libplug != NULL)
+			break;
+		usleep(100000);
+	}
+
 	if (libplug == NULL) {
 		fprintf(stderr, "failed to load libplug\n");
 		exit(1);
@@ -47,10 +58,30 @@ void libplug_reload(void)
 	printf("reloading!\n");
 }
 
+int plug_should_reload(time_t *last_modified_time)
+{
+	int ret = 0;
+	struct stat attr;
+
+	if (stat(lib_plug_file_name, &attr)) {
+		fprintf(stderr, "failed to stat %s\n", strerror(errno));
+		exit(1);
+	}
+
+	if (attr.st_mtime != *last_modified_time) {
+		*last_modified_time = attr.st_mtime;
+		ret = 1;
+	}
+
+	return ret;
+}
+
 int main(void)
 {
 	libplug_reload();
 	size_t factor = 80;
+	time_t last_modified_time = {0};
+	plug_should_reload(&last_modified_time);
 
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_ALWAYS_RUN);
 	InitWindow(factor*16, factor*9, "balancer");
@@ -58,7 +89,7 @@ int main(void)
 
 	plug_init(&plug);
 	while (!WindowShouldClose()) {
-		if (IsKeyPressed(KEY_R)) {
+		if (plug_should_reload(&last_modified_time)) {
 			plug_pre_reload(&plug);
 			libplug_reload();
 			plug_post_reload(&plug);
